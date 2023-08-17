0e34c3eb951ee8faee5b2a94d5a48427a507d227
zqiuliu <1370192111@qq.com>
2023-08-17 18:12:07 +0800

    test

---
+++ AAAM.py
@@ -0,0 +1,81 @@
+import os
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from torch import tensor
+import random
+
+__all__ = ['AAAM']
+
+
+class AAAM:
+    def __init__(
+            self,
+            eps: float = 16 / 255,
+            eta: float = 0.1,
+            alpha: float = 1.6 / 255,
+            device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    ):
+        self.eps = eps
+        self.alpha = alpha
+        self.device = device
+        self.eta = eta
+        self.criterion = nn.MSELoss(reduction='mean')
+        self.seed_torch(0)
+
+    def seed_torch(self, seed):
+        """Set a random seed to ensure that the results are reproducible"""
+        random.seed(seed)
+        os.environ['PYTHONHASHSEED'] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.enabled = False
+
+    def LossCosine(self, I_y1: tensor, I_y2: tensor) -> tensor:
+        """
+        args:
+        I_y1: 第一正确标签图像的SGLRP hot_map
+        I_y2: 第二正确标签图像的SGLRP hot_map
+        """
+        temp = torch.dot(I_y1, I_y2)
+        norm = torch.norm(I_y1) * torch.norm(I_y2)
+        L_cosine = temp / norm
+        L = -  torch.log((1 - L_cosine) / 2)
+        return L
+
+    def Loss(self, model: nn.Module, x: tensor, y: tensor, cam1: tensor, cam2: tensor) -> tensor:
+        pred = model(x)
+        loss = F.nll_loss(pred, y)  # PGD loss
+        gamma = 1
+        loss = self.LossCosine(cam1, cam2) - gamma * loss
+        return loss
+
+    @staticmethod
+    def calculate_cam(self):
+        pass
+
+    def __call__(self, model, images, labels, *args, **kwargs):
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+        adv = images.clone().detach()
+        n = 0
+
+        while torch.sqrt(self.criterion(images, adv)) < self.eta:
+            cam1 = ...
+            cam2 = ...
+            loss = self.Loss(model, images, labels, cam1, cam2)
+            gt1 = torch.autograd.grad(loss, images, retain_graph=True)[0]
+            gt1 = images.shape[0] * gt1 / torch.norm(gt1, p=1) + gt1 / torch.norm(gt1, p=2)
+            gt1 = gt1 / 2
+            for i in range(4):
+                adv = adv / 2 ** i
+            adv = adv - self.alpha * gt1
+            n = n + 1
+            delta = torch.clip(adv - images, -self.eps, self.eps)
+            adv = torch.clip(images + delta, 0, 1)
+        return adv
--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 对抗样本算法仓库(备用)
 
 TAIG: TRANSFERABLE ADVERSARIAL ATTACK BASED ON INTEGRATED GRADIENTS <br>
-FIA： Feature Importance-aware Transferable Adversarial Attacks
+FIA： Feature Importance-aware Transferable Adversarial Attacks <br>
+AAAM: An Interpretive Adversarial Attack Method: Attacking Softmax Gradient Layer-Wise Relevance Propagation Based on Cosine Similarity Constraint and TS-Invariant
