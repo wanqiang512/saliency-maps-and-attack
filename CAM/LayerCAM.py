3950338f2ff897842a4cbcdce714a3f4f727ae44
wanqiang512 <1370192111@qq.com>
2023-09-12 20:26:36 +0800

    Add files via upload

---
+++ CAM/LayerCAM.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn.functional as F
+from statistics import mode, mean
+import torch.nn as nn
+from torchvision.transforms import Normalize
+
+
+def TNormalize(x, IsRe=False, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    if not IsRe:
+        x = Normalize(mean=mean, std=std)(x)
+    elif IsRe:
+        # tensor.shape:(3,w.h)
+        for idx, i in enumerate(std):
+            x[:, idx, :, :] *= i
+        for index, j in enumerate(mean):
+            x[:, index, :, :] += j
+    return x
+
+
+class SaveValues():
+    def __init__(self, m):
+        # register a hook to save values of activations and gradients
+        self.activations = None
+        self.gradients = None
+        self.forward_hook = m.register_forward_hook(self.hook_fn_act)
+        self.backward_hook = m.register_backward_hook(self.hook_fn_grad)
+
+    def hook_fn_act(self, module, input, output):
+        self.activations = output
+
+    def hook_fn_grad(self, module, grad_input, grad_output):
+        self.gradients = grad_output[0]
+
+    def remove(self):
+        self.forward_hook.remove()
+        self.backward_hook.remove()
+
+
+class CAM(object):
+    """ Class Activation Mapping """
+
+    def __init__(self, model, target_layer):
+        """
+        Args:
+            model: a base model to get CAM which have global pooling and fully connected layer.
+            target_layer: conv_layer before Global Average Pooling
+        """
+
+        self.model = model
+        self.target_layer = target_layer
+
+        # save values of activations and gradients in target_layer
+        self.values = SaveValues(self.target_layer)
+
+    def forward(self, x, idx=None):
+        """
+        Args:
+            x: input image. shape =>(1, 3, H, W)
+        Return:
+            heatmap: class activation mappings of the predicted class
+        """
+
+        # object classification
+        score = self.model(TNormalize(x))
+
+        prob = F.softmax(score, dim=1)
+
+        if idx is None:
+            prob, idx = torch.max(prob, dim=1)
+            idx = idx.item()
+            prob = prob.item()
+            print("predicted class ids {}\t probability {}".format(idx, prob))
+
+        # cam can be calculated from the weights of linear layer and activations
+        weight_fc = list(
+            self.model._modules.get('fc').parameters())[0].to('cpu').data
+
+        cam = self.getCAM(self.values, weight_fc, idx)
+
+        return cam, idx
+
+    def __call__(self, x):
+        return self.forward(x)
+
+    def getCAM(self, values, weight_fc, idx):
+        '''
+        values: the activations and gradients of target_layer
+            activations: feature map before GAP.  shape => (1, C, H, W)
+        weight_fc: the weight of fully connected layer.  shape => (num_classes, C)
+        idx: predicted class id
+        cam: class activation map.  shape => (1, num_classes, H, W)
+        '''
+
+        cam = F.conv2d(values.activations, weight=weight_fc[:, :, None, None])
+        _, _, h, w = cam.shape
+
+        # class activation mapping only for the predicted class
+        # cam is normalized with min-max.
+        cam = cam[:, idx, :, :]
+        cam -= torch.min(cam)
+        cam /= torch.max(cam)
+        cam = cam.view(1, 1, h, w)
+
+        return cam.data
+
+
+class LayerCAM(CAM):
+    def __init__(self, model, target_Layer):
+        super().__init__(model, target_Layer)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def forward(self, x, idx=None, ):
+        """
+              Args:
+                   x: input image. shape =>(1, 3, H, W)
+                   idx: the index of the target class
+               Return:
+                   heatmap: class activation mappings of predicted classes
+        """
+        b, c, h, w = x.size()
+        logits = self.model(TNormalize(x))
+
+        if idx is None:
+            idx = torch.argmax(logits, dim=1).to(self.device)
+
+        one_hot = F.one_hot(idx, len(logits[0]))
+        logits = F.softmax(logits, dim=1)
+        self.model.zero_grad()
+        logits.backward(gradient=one_hot, retain_graph=False)
+        activation = self.values.activations.clone().detach()
+        gradients = self.values.gradients.clone().detach()
+        with torch.no_grad():
+            activation_maps = activation * F.relu(gradients)
+            cam = torch.sum(activation_maps, dim=1, keepdim=True)
+            cam = F.interpolate(cam, size=(h, w), mode='bilinear', align_corners=False)
+            cam = F.relu(cam)
+            cam = cam - cam.min() / (cam.max() - cam.min())
+        return cam
