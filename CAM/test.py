1d4d97b5c13ed3210463aea3d520f87ae2d5894f
wanqiang512 <1370192111@qq.com>
2023-09-12 15:36:57 +0800

    Add files via upload

--- CAM/test.py
+++ CAM/test.py
@@ -1,28 +1,41 @@
+from matplotlib import pyplot as plt
 from torchvision.io.image import read_image
 from torchvision.transforms.functional import normalize, resize, to_pil_image
 
-x = read_image("test.jpg").unsqueeze(0)
-x = resize(x, (224, 224)) / 255.0
+x = read_image("0.jpg")
+img = x
+x = (resize(x, (224, 224)) / 255.0).unsqueeze(0)
 x = normalize(x, [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 
-from torchvision.models import resnet50
-from torchcam.methods import GradCAMpp
-
-model = resnet50(pretrained=True).eval()
-cam = GradCAMpp(model=model, target_layer="layer4")
+from torchvision.models import resnet18
+# from torchcam.methods import GradCAMpp
+#
+model = resnet18(pretrained=True).eval()
 out = model(x)
-grayscale_cam = cam(out.argmax(dim=1).item(), out)
-print(grayscale_cam[0])
-import matplotlib.pyplot as plt
-
-# Visualize the raw CAM
-plt.imshow(grayscale_cam[0].squeeze().numpy());
-plt.axis('off');
-plt.tight_layout();
-plt.show()
+# cam = GradCAMpp(model=model)
+# grayscale_cam = cam(out.argmax(dim=1).item(), out)
+# print(grayscale_cam[0])
+# import matplotlib.pyplot as plt
+#
+# # Visualize the raw CAM
+# plt.imshow(grayscale_cam[0].squeeze().numpy());
+# plt.axis('off');
+# plt.tight_layout();
+# plt.show()
 
 from GradCAMplusplus import GradCamplusplus
 
 cam = GradCamplusplus(model)
 test = cam.get_gradient(x, "layer4", out.argmax(dim=1).item())
 print(test)
+# Visualize the raw CAM
+plt.imshow(test[0].squeeze().numpy());
+plt.axis('off');
+plt.tight_layout();
+plt.show()
+
+from torchcam.utils import  overlay_mask
+# Resize the CAM and overlay it
+result = overlay_mask(to_pil_image(img), to_pil_image(test[0].squeeze(0), mode='F'), alpha=0.5)
+# Display it
+plt.imshow(result); plt.axis('off'); plt.tight_layout(); plt.show()
