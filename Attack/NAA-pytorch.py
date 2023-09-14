74bc3ff88f1d553230720257c487b861bbc27084
wanqiang512 <1370192111@qq.com>
2023-09-14 20:09:25 +0800

    Add files via upload

--- Attack/NAA-pytorch.py
+++ Attack/NAA-pytorch.py
@@ -26,7 +26,7 @@ class NAA:
         loss = 0
         gamma = 1.0
         for idx, model in enumerate(models):
-            logits = model(x)
+            logits = model(self.TNormalize(x))
             attribution = (self.feature_map[idx] - base_feature[idx]) * weights[idx]
             blank = torch.zeros_like(attribution)
             positive = torch.where(attribution >= 0, attribution, blank)
@@ -49,7 +49,7 @@ class NAA:
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.enabled = False
 
-    def TNormalize(self, x, IsRe, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    def TNormalize(self, x, IsRe=False, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
         if not IsRe:
             x = Normalize(mean=mean, std=std)(x)
         elif IsRe:
@@ -66,7 +66,11 @@ class NAA:
     def backward_hook(self, model, grad_input, grad_output):
         self.weight.append(grad_output[0])
 
-    def __call__(self, models, layers, inputs, labels, clip_min=-1, clip_max=1):
+    def __call__(self, models, layers, inputs, labels):
+        if torch.max(inputs) > 1 or torch.min(inputs) < 0:
+            raise ValueError('Input must have a range [0, 1] (max: {}, min: {})'.format(
+                torch.max(inputs), torch.min(inputs))
+            )
         self.weight.clear()
         self.feature_map.clear()
         for model, layer in zip(models, layers):
@@ -92,14 +96,13 @@ class NAA:
                     for l in range(self.ens):
                         self.feature_map.clear()
                         x_base = torch.zeros_like(inputs)
-                        x_base = self.TNormalize(x_base, IsRe=False)
                         temp_noise = np.random.normal(size=inputs.shape, loc=0.0, scale=0.2)
                         temp_noise = torch.from_numpy(temp_noise).to(self.device, dtype=torch.float32)
-                        image_tmp = torch.clip(inputs.clone() + temp_noise, -1, 1)
+                        image_tmp = torch.clip(inputs.clone() + temp_noise, 0, 1)
                         image_tmp = (image_tmp * (1 - l / self.ens) + (l / self.ens) * x_base)
-                        logits = model(image_tmp)
+                        logits = model(self.TNormalize(image_tmp))
                         logits = nn.functional.softmax(logits, 1)
-                        labels_onehot = torch.nn.functional.one_hot(labels, num_classes=len(logits[0])).float()
+                        labels_onehot = nn.functional.one_hot(labels, num_classes=len(logits[0])).float()
                         score = logits * labels_onehot
                         loss = torch.sum(score)
                         loss.backward()
@@ -113,11 +116,10 @@ class NAA:
                     weights[idx] = weight_tensor
 
             base_line = torch.zeros_like(inputs)
-            base_line = self.TNormalize(base_line, IsRe=False)
             self.feature_map.clear()
             base_feature = []
             for model in models:
-                model(base_line)
+                model(self.TNormalize(base_line))
             for fm in self.feature_map:
                 base_feature.append(fm)
             self.feature_map.clear()
@@ -127,13 +129,7 @@ class NAA:
             adv.grad.data.zero_()
             g = self.u * g + (adv_grad / (torch.mean(torch.abs(adv_grad), [1, 2, 3], keepdim=True)))
             adv = adv + a * torch.sign(g)
-            if clip_min is None and clip_max is None:
-                adv = self.TNormalize(adv, IsRe=True)
-                adv = torch.clip(adv, 0, 1)
-                adv = self.TNormalize(adv, IsRe=False)
-            else:
-                adv = torch.clip(adv, clip_min, clip_max)
             diff = adv - inputs
             noise = torch.clip(diff, -self.esp, self.esp)
-            adv = (adv + noise).detach_()
+            adv = torch.clip(adv + noise, 0, 1).detach_()
         return adv
