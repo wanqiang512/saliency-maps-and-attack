0353a2b5b4ca73f57e47dfa91ccf9ce1cbb9a064
wanqiang512 <1370192111@qq.com>
2023-09-14 21:43:40 +0800

    Update NAA-pytorch.py

--- Attack/NAA-pytorch.py
+++ Attack/NAA-pytorch.py
@@ -21,20 +21,19 @@ class NAA:
         self.weight = []
         self.seed_torch(1024)
 
-    def get_NAA_loss(self, x, models, weights, base_feature):
+    def get_NAA_loss(self, x, model, weight, base_feature):
         self.feature_map.clear()
         loss = 0
         gamma = 1.0
-        for idx, model in enumerate(models):
-            logits = model(self.TNormalize(x))
-            attribution = (self.feature_map[idx] - base_feature[idx]) * weights[idx]
-            blank = torch.zeros_like(attribution)
-            positive = torch.where(attribution >= 0, attribution, blank)
-            negative = torch.where(attribution < 0, attribution, blank)
-            positive = positive
-            negative = negative
-            balance_attribution = positive + gamma * negative
-            loss += torch.sum(balance_attribution) / balance_attribution.numel()
+        logits = model(self.TNormalize(x))
+        attribution = (self.feature_map[0] - base_feature) * weight
+        blank = torch.zeros_like(attribution)
+        positive = torch.where(attribution >= 0, attribution, blank)
+        negative = torch.where(attribution < 0, attribution, blank)
+        positive = positive
+        negative = negative
+        balance_attribution = positive + gamma * negative
+        loss = torch.sum(balance_attribution) / balance_attribution.numel()
         return loss
 
     def seed_torch(self, seed):
@@ -66,23 +65,21 @@ class NAA:
     def backward_hook(self, model, grad_input, grad_output):
         self.weight.append(grad_output[0])
 
-    def __call__(self, models, layers, inputs, labels):
+    def __call__(self, model, inputs, labels, layer):
         if torch.max(inputs) > 1 or torch.min(inputs) < 0:
             raise ValueError('Input must have a range [0, 1] (max: {}, min: {})'.format(
                 torch.max(inputs), torch.min(inputs))
             )
         self.weight.clear()
         self.feature_map.clear()
-        for model, layer in zip(models, layers):
-            if hasattr(model, layer):
-                getattr(model, layer).register_backward_hook(self.backward_hook)
-                getattr(model, layer).register_forward_hook(self.forward_hook)
-            else:
-                raise Exception(f'can not find {layer} in {model.__class__.__name__}')
+        if hasattr(model, layer):
+            getattr(model, layer).register_backward_hook(self.backward_hook)
+            getattr(model, layer).register_forward_hook(self.forward_hook)
+        else:
+            raise Exception(f'can not find {layer} in {model.__class__.__name__}')
 
         a = self.a
         g = torch.zeros_like(inputs)
-        weights = []
         images = inputs.clone().detach()
         labels = labels.clone().detach()
         adv = images.clone().detach()
@@ -90,40 +87,36 @@ class NAA:
         for i in range(self.steps):
             adv.requires_grad = True
             if i == 0:
-                for model in models:
-                    temp_weight = 0
-                    self.weight.clear()
-                    for l in range(self.ens):
-                        self.feature_map.clear()
-                        x_base = torch.zeros_like(inputs)
-                        temp_noise = np.random.normal(size=inputs.shape, loc=0.0, scale=0.2)
-                        temp_noise = torch.from_numpy(temp_noise).to(self.device, dtype=torch.float32)
-                        image_tmp = torch.clip(inputs.clone() + temp_noise, 0, 1)
-                        image_tmp = (image_tmp * (1 - l / self.ens) + (l / self.ens) * x_base)
-                        logits = model(self.TNormalize(image_tmp))
-                        logits = nn.functional.softmax(logits, 1)
-                        labels_onehot = nn.functional.one_hot(labels, num_classes=len(logits[0])).float()
-                        score = logits * labels_onehot
-                        loss = torch.sum(score)
-                        loss.backward()
-                    for w in self.weight:
-                        temp_weight += w
-                    weights.append(temp_weight)
-                for idx, weight_tensor in enumerate(weights):
-                    weight_tensor = weight_tensor.to(self.device)
-                    square = torch.sum(torch.square(weight_tensor), [1, 2, 3], keepdim=True)
-                    weight_tensor = -weight_tensor / torch.sqrt(square)
-                    weights[idx] = weight_tensor
+                temp_weight = 0
+                self.weight.clear()
+                for l in range(self.ens):
+                    self.feature_map.clear()
+                    x_base = torch.zeros_like(inputs)
+                    temp_noise = np.random.normal(size=inputs.shape, loc=0.0, scale=0.2)
+                    temp_noise = torch.from_numpy(temp_noise).to(self.device, dtype=torch.float32)
+                    image_tmp = torch.clip(inputs.clone() + temp_noise, 0, 1)
+                    image_tmp = (image_tmp * (1 - l / self.ens) + (l / self.ens) * x_base)
+                    logits = model(self.TNormalize(image_tmp))
+                    logits = nn.functional.softmax(logits, 1)
+                    labels_onehot = nn.functional.one_hot(labels, num_classes=len(logits[0])).float()
+                    score = logits * labels_onehot
+                    loss = torch.sum(score)
+                    loss.backward()
+                for w in self.weight:
+                    temp_weight += w
 
+                weight = temp_weight.to(self.device).clone().detach()
+                square = torch.sum(torch.square(weight), [1, 2, 3], keepdim=True)
+                weight = -weight / torch.sqrt(square)
+
+            self.feature_map.clear()
             base_line = torch.zeros_like(inputs)
-            self.feature_map.clear()
-            base_feature = []
-            for model in models:
-                model(self.TNormalize(base_line))
+            base_feature = 0
+            logits = model(self.TNormalize(base_line))
             for fm in self.feature_map:
-                base_feature.append(fm)
+                base_feature += fm
             self.feature_map.clear()
-            loss = self.get_NAA_loss(adv, models, weights, base_feature)
+            loss = self.get_NAA_loss(adv, model, weight, base_feature)
             loss.backward()
             adv_grad = adv.grad.clone()
             adv.grad.data.zero_()
