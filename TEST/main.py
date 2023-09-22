1b57abeb3209e121ae43c50a64843f343beb47fd
wanqiang512 <1370192111@qq.com>
2023-09-22 16:23:54 +0800

    Add files via upload

--- TEST/main.py
+++ TEST/main.py
@@ -8,16 +8,16 @@ from torch.utils.data import DataLoader
 from Normalize import Normalize, TfNormalize
 from loader import ImageNet
 from torch_nets import (
-    tf_inception_v3,
-    tf_inception_v4,
-    tf_resnet_v2_50,
-    tf_resnet_v2_101,
-    tf_resnet_v2_152,
-    tf_inc_res_v2,
-    tf_adv_inception_v3,
-    tf_ens3_adv_inc_v3,
-    tf_ens4_adv_inc_v3,
-    tf_ens_adv_inc_res_v2,
+    tf2torch_inception_v3,
+    tf2torch_inception_v4,
+    tf2torch_resnet_v2_50,
+    tf2torch_resnet_v2_101,
+    tf2torch_resnet_v2_152,
+    tf2torch_inc_res_v2,
+    tf2torch_adv_inception_v3,
+    tf2torch_ens3_adv_inc_v3,
+    tf2torch_ens4_adv_inc_v3,
+    tf2torch_ens_adv_inc_res_v2,
 )
 
 parser = argparse.ArgumentParser()
@@ -34,25 +34,25 @@ def get_model(net_name, model_dir):
     model_path = os.path.join(model_dir, net_name + '.npy')
 
     if net_name == 'tf_inception_v3':
-        net = tf_inception_v3
+        net = tf2torch_inception_v3
     elif net_name == 'tf_inception_v4':
-        net = tf_inception_v4
+        net = tf2torch_inception_v4
     elif net_name == 'tf_resnet_v2_50':
-        net = tf_resnet_v2_50
+        net = tf2torch_resnet_v2_50
     elif net_name == 'tf_resnet_v2_101':
-        net = tf_resnet_v2_101
+        net = tf2torch_resnet_v2_101
     elif net_name == 'tf_resnet_v2_152':
-        net = tf_resnet_v2_152
+        net = tf2torch_resnet_v2_152
     elif net_name == 'tf_inc_res_v2':
-        net = tf_inc_res_v2
+        net = tf2torch_inc_res_v2
     elif net_name == 'tf_adv_inception_v3':
-        net = tf_adv_inception_v3
+        net = tf2torch_adv_inception_v3
     elif net_name == 'tf_ens3_adv_inc_v3':
-        net = tf_ens3_adv_inc_v3
+        net = tf2torch_ens3_adv_inc_v3
     elif net_name == 'tf_ens4_adv_inc_v3':
-        net = tf_ens4_adv_inc_v3
+        net = tf2torch_ens4_adv_inc_v3
     elif net_name == 'tf_ens_adv_inc_res_v2':
-        net = tf_ens_adv_inc_res_v2
+        net = tf2torch_ens_adv_inc_res_v2
     else:
         print('Wrong model name!')
 
@@ -84,7 +84,7 @@ def main():
     model_names = ['tf_inception_v3', 'tf_inception_v4', 'tf_inc_res_v2', 'tf_resnet_v2_152', 'tf_resnet_v2_50',
                    'tf_resnet_v2_101', 'tf_adv_inception_v3', 'tf_ens3_adv_inc_v3', 'tf_ens4_adv_inc_v3',
                    'tf_ens_adv_inc_res_v2']
-    models_path = './models/'
+    models_path = '../torch_nets_weight/'
     for model_name in model_names:
         verify(model_name, models_path)
         print("===================================================")
