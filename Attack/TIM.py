1bc828fc122a216ed2918d7abea6826134672be8
wanqiang512 <1370192111@qq.com>
2023-09-30 16:16:52 +0800

    Add files via upload

---
+++ Attack/TIM.py
@@ -0,0 +1,146 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from scipy import stats as st
+from torchvision.transforms import Normalize
+
+
+# TIDIM
+class TIM:
+    r"""
+    Arguments:
+        model (nn.Module): model to attack.
+        eps (float): maximum perturbation. (Default: 8/255)
+        alpha (float): step size. (Default: 2/255)
+        steps (int): number of iterations. (Default: 10)
+        decay (float): momentum factor. (Default: 0.0)
+        kernel_name (str): kernel name. (Default: gaussian)
+        len_kernel (int): kernel length.  (Default: 15, which is the best according to the paper)
+        nsig (int): radius of gaussian kernel. (Default: 3; see Section 3.2.2 in the paper for explanation)
+        resize_rate (float): resize factor used in input diversity. (Default: 0.9)
+        diversity_prob (float) : the probability of applying input diversity. (Default: 0.5)
+    Examples::
+        >>> attack = TIM(model, eps=8/255, alpha=2/255, steps=10, decay=1.0, resize_rate=0.9, diversity_prob=0.7)
+    """
+
+    def __init__(
+            self,
+            model,
+            device=None,
+            eps=16 / 255,
+            alpha=1.6 / 255,
+            steps=10,
+            decay=1.0,
+            kernel_name='gaussian',
+            len_kernel=7,
+            nsig=3,
+            resize_rate=0.9,
+            diversity_prob=0.5
+    ):
+        self.eps = eps
+        self.steps = steps
+        self.decay = decay
+        self.alpha = alpha
+        self.resize_rate = resize_rate
+        self.diversity_prob = diversity_prob
+        self.kernel_name = kernel_name
+        self.len_kernel = len_kernel
+        self.nsig = nsig
+        self.stacked_kernel = torch.from_numpy(self.kernel_generation())
+        self.model = model
+        self.device = device
+
+    def TNormalize(self, x, IsRe=False, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+        if not IsRe:
+            x = Normalize(mean=mean, std=std)(x)
+        elif IsRe:
+            # tensor.shape:(3,w.h)
+            for idx, i in enumerate(std):
+                x[:, idx, :, :] *= i
+            for index, j in enumerate(mean):
+                x[:, index, :, :] += j
+        return x
+
+    def forward(self, images, labels):
+        images = images.clone().detach().to(self.device)
+        labels = labels.clone().detach().to(self.device)
+        adv_images = images.clone().detach()
+        loss = nn.CrossEntropyLoss()
+        momentum = torch.zeros_like(images).detach().to(self.device)
+        stacked_kernel = self.stacked_kernel.to(self.device)
+
+        for _ in range(self.steps):
+            adv_images.requires_grad = True
+            outputs = self.model(self.TNormalize(adv_images))
+            cost = loss(outputs, labels)
+            # Update adversarial images
+            grad = torch.autograd.grad(cost, adv_images, retain_graph=False, create_graph=False)[0]
+            # depth wise conv2d
+            grad = F.conv2d(grad, stacked_kernel, stride=1, padding='same', groups=3)
+            grad = grad / torch.mean(torch.abs(grad), dim=(1, 2, 3), keepdim=True)
+            grad = grad + momentum * self.decay
+            momentum = grad
+            adv_images = adv_images.detach() + self.alpha * grad.sign()
+            delta = torch.clamp(adv_images - images, min=-self.eps, max=self.eps)
+            adv_images = torch.clamp(images + delta, min=0, max=1).detach()
+        return adv_images
+
+    def kernel_generation(self):
+        if self.kernel_name == 'gaussian':
+            kernel = self.gkern(self.len_kernel, self.nsig).astype(np.float32)
+        elif self.kernel_name == 'linear':
+            kernel = self.lkern(self.len_kernel).astype(np.float32)
+        elif self.kernel_name == 'uniform':
+            kernel = self.ukern(self.len_kernel).astype(np.float32)
+        else:
+            raise NotImplementedError
+
+        stack_kernel = np.stack([kernel, kernel, kernel])
+        stack_kernel = np.expand_dims(stack_kernel, 1)
+        return stack_kernel
+
+    def gkern(self, kernlen=15, nsig=3):
+        """Returns a 2D Gaussian kernel array."""
+        x = np.linspace(-nsig, nsig, kernlen)
+        kern1d = st.norm.pdf(x)
+        kernel_raw = np.outer(kern1d, kern1d)
+        kernel = kernel_raw / kernel_raw.sum()
+        return kernel
+
+    def ukern(self, kernlen=15):
+        kernel = np.ones((kernlen, kernlen)) * 1.0 / (kernlen * kernlen)
+        return kernel
+
+    def lkern(self, kernlen=15):
+        kern1d = 1 - np.abs(np.linspace((-kernlen + 1) / 2,
+                                        (kernlen - 1) / 2, kernlen) / (kernlen + 1) * 2)
+        kernel_raw = np.outer(kern1d, kern1d)
+        kernel = kernel_raw / kernel_raw.sum()
+        return kernel
+
+    def input_diversity(self, x):
+        img_size = x.shape[-1]
+        img_resize = int(img_size * self.resize_rate)
+
+        if self.resize_rate < 1:
+            img_size = img_resize
+            img_resize = x.shape[-1]
+
+        rnd = torch.randint(low=img_size, high=img_resize,
+                            size=(1,), dtype=torch.int32)
+        rescaled = F.interpolate(
+            x, size=[rnd, rnd], mode='bilinear', align_corners=False)
+        h_rem = img_resize - rnd
+        w_rem = img_resize - rnd
+        pad_top = torch.randint(low=0, high=h_rem.item(),
+                                size=(1,), dtype=torch.int32)
+        pad_bottom = h_rem - pad_top
+        pad_left = torch.randint(
+            low=0, high=w_rem.item(), size=(1,), dtype=torch.int32)
+        pad_right = w_rem - pad_left
+
+        padded = F.pad(rescaled, [pad_left.item(), pad_right.item(
+        ), pad_top.item(), pad_bottom.item()], value=0)
+
+        return padded if torch.rand(1) < self.diversity_prob else x
