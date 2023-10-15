7c5b6712e299857c50d96bc267c6c9e47d0d01db
wanqiang512 <1370192111@qq.com>
2023-10-15 13:53:37 +0800

    Add files via upload

--- Attack/TAIG.py
+++ Attack/TAIG.py
@@ -3,10 +3,9 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
+from torchvision.transforms import Normalize
 
 __all__ = ['TAIG']
-
-from torchvision.transforms import Normalize
 
 
 # model must be nn.Sequential {Normalized(mean,std), model}
@@ -14,23 +13,25 @@ class TAIG:
     def __init__(
             self,
             eps: float = 16 / 255,
-            ens: int = 10,
-            alpha: float = 1 / 255,
+            ens: int = 20,
+            alpha: float = 1.6 / 255,
             R: bool = True,
             iters: int = 10,
             device: torch.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+            u=1,
     ):
         """TAIG-S and TAIG-R
         >>> attack = TAIG(...)
         >>> adv = attack(...)
         """
+        self.u = u
         self.eps = eps
         self.ens = ens
         self.alpha = alpha
         self.R = R
         self.iters = iters
         self.device = device
-        self.seed_torch(1024)
+        self.seed_torch(1234)
 
     def seed_torch(self, seed):
         """Set a random seed to ensure that the results are reproducible"""
@@ -44,7 +45,7 @@ class TAIG:
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.enabled = False
 
-    def TNormalize(self, x, IsRe=False, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    def TNormalize(self, x, IsRe=False, mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]):
         if not IsRe:
             x = Normalize(mean=mean, std=std)(x)
         elif IsRe:
@@ -57,7 +58,7 @@ class TAIG:
 
     def compute_ig(self, model, images, labels):
         baseline = torch.zeros_like(images)
-        scaled_inputs = [baseline + (float(i) / self.iters) * (images - baseline) for i in range(0, self.iters + 1)]
+        scaled_inputs = [baseline + (float(i) / self.ens) * (images - baseline) for i in range(0, self.ens + 1)]
         scaled_inputs = torch.stack(scaled_inputs).to(self.device, dtype=torch.float32)
         if self.R:
             temp = np.random.uniform(-self.eps, self.eps, scaled_inputs.shape)
@@ -72,8 +73,7 @@ class TAIG:
             score = logits[:, temp_label]
             loss = torch.mean(score)
             model.zero_grad()
-            loss.backward()
-            grad = temp_image.grad.data
+            grad = torch.autograd.grad(loss, temp_image, retain_graph=False, create_graph=False)[0]
             avg_grad = torch.mean(grad, dim=0)
             IG.append((temp_image[-1] - temp_image[0]) * avg_grad)
         IG = torch.stack(IG).to(self.device)
@@ -83,10 +83,12 @@ class TAIG:
         images = images.clone().detach().to(self.device)
         labels = labels.clone().detach().to(self.device)
         adv = images.clone().detach()
+        g = torch.zeros_like(images)
 
-        for i in range(self.ens):
+        for i in range(self.iters):
             ig = self.compute_ig(model, adv, labels)
-            adv = adv.detach() - self.alpha * torch.sign(ig)
+            g = self.u * g + ig / torch.mean(torch.abs(ig), dim=(1, 2, 3), keepdim=True)
+            adv = adv.detach() - self.alpha * torch.sign(g)
             adv = torch.where(adv > images + self.eps, images + self.eps, adv)
             adv = torch.where(adv < images - self.eps, images - self.eps, adv)
             adv = torch.clip(adv, 0, 1)
