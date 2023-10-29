6e4f2b7c3b16934126e94292de36769b2b1d812d
wanqiang512 <1370192111@qq.com>
2023-10-29 17:29:06 +0800

    test

--- adversarial_attack/__init__.py
+++ adversarial_attack/__init__.py
@@ -4,7 +4,7 @@
 @file: __init__.py
 @time: 2023/10/15 23:15
 """
-from adversarial_attack import AAAM, FIA, FGSM, IFGSM, PGD, PIM, RPA, NAA, SSA, TAIG, TIM, Admix, SFVA, DIM, SVD, SIM, \
+from adversarial_attack import AAAM, FIA, FGSM, IFGSM, PGD, PIM, RPA, NAA, SSA, TAIG, TIM, test, SFVA, DIM, SVD, SIM, \
     MIFGSM
 
 try:
